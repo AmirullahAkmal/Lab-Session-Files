15bc4624c360b7e27f57ed4fe2036823dc3b6487
Amirullah Akmal <55593484+AmirullahAkmal@users.noreply.github.com>
2019-09-21 02:02:46 +0800

    Update HelloMalaysia.cpp

--- HelloMalaysia.cpp
+++ HelloMalaysia.cpp
@@ -1,11 +1,15 @@
-#include<iostream>
-using namespace std;
-int main()
+##Created by Amirullah
+##This is example of your first C++ program file
+
+#include<iostream> /*This part is to allow any input and output operations to be executed
+Without this, the file cannot be read by the code runner */
+using namespace std; /* This part is to allow the use of C++ standard library, where those functions in this library will
+be declared */
+
+int main() /* this part to declare the function, main function of the file */
 {
-  string name;
-cout<<"Hello Malaysia"<<endl;
-cout<<"What is your name?: ";
-cin>>name;
-cout<<name<<endl;
-  return 0;
+cout<<"Hello Malaysia"<<endl; /* cout is character output, where any character, integer, and string is readable once executed. The << part is 
+what follows the command. In this case, this line commanded "Hello Malaysia" to be output of the program. "endl" is end line, which acts like
+"Enter". Semicolon is to end the statement and its essential */
+  return 0; //return 0 is like "Okay, your file is running fine //
 }
